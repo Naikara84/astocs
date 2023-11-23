0fb5b9aebec174ebae006a3068774d6787eb5458
Timothy Naikara <silvernathan84@gmail.com>
2023-11-23 16:11:39 +0300

    First commit

---
+++ server/Routes/AdminRoute.js
@@ -0,0 +1,21 @@
+import express from 'express'
+import con from '../utils/db.js'
+const router = express.Router()
+import jwt from 'jsonwebtoken'
+router.post('/adminlogin', (req, res) => {
+    console.log(res)
+    const sql = "SELECT * from employees where email =? and password = ?"
+    con.query(sql, [req.body.email, req.body.password], (err, result) => {
+        
+        if (err) return res.json({ loginStatus: false, Error: "Query error" });
+        if (result.length > 0) {   
+            const email = result[0].email;
+            const token = jwt.sign({ role: "admin", email: email, id: result[0].id }, "jwt_secret_key", { expiresIn: '1d' })
+            res.cookie('token', token)
+            return res.json({ loginStatus: true });
+        }else{
+         return res.json({ loginStatus: false, Error:"wrong email or password" });
+        }
+    })
+})
+export { router as adminRouter }
